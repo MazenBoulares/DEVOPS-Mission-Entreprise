5880e6d7a8381058f790b5a7a72b9509b9a9baf3
MazenBoulares <88446502+MazenBoulares@users.noreply.github.com>
2025-07-16 20:22:37 +0100

    Update BlocServiceTest.java

--- src/test/java/tn/esprit/spring/BlocServiceTest.java
+++ src/test/java/tn/esprit/spring/BlocServiceTest.java
@@ -1,57 +1,59 @@
 package tn.esprit.spring;
 
-import org.junit.After;
 import org.junit.jupiter.api.*;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-@RunWith(SpringRunner.class)
-@TestMethodOrder(MethodOrderer.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BlocServiceTest {
 
     @BeforeAll
-    void bedore() {
-
+    void before() {
+        System.out.println("Before all tests");
     }
 
     @AfterAll
     void after() {
-
+        System.out.println("After all tests");
     }
 
     @BeforeEach
     void beforeEach() {
-
+        System.out.println("Before each test");
     }
 
     @AfterEach
     void afterEach() {
-
+        System.out.println("After each test");
     }
 
     @Order(1)
     @RepeatedTest(4)
     void test() {
-
+        Assertions.assertTrue(true);
     }
 
     @Order(4)
     @Test
     void test2() {
-
+        Assertions.assertEquals(1, 1);
     }
 
     @Order(2)
     @Test
     void test3() {
-
+        Assertions.assertNotNull("Hello");
     }
 
     @Order(3)
     @Test
     void test4() {
-
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            int x = 1 / 0;
+        });
     }
 }
